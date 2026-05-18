ef1a9a5b8fca5e8ed817a819d2b48606d1cba156
chinjearambasa-png <chinjear.ambasa@fcpc.edu.ph>
2026-05-18 14:55:12 +0800

    Update and rename README.md to user_guide.py
    user _guide.md

---
+++ user_guide.py
@@ -0,0 +1,141 @@
+Banking Card  System User Guide"
+
+
+Introduction
+
+The Banking Card System is a Python based console application designed to help users manage their digital money transactions securely and conveniently. The system allows users to log in using authorized accounts, check balances, convert currencies between PHP and USD, cash in funds, and transfer money using the Padala Pay feature. It also records transactions and displays receipts for successful operations.
+
+
+
+Objectives
+
+The system aims to:
+
+* Provide secure login authentication
+
+* Manage PHP and USD balances
+
+* Convert money between currencies
+
+* Allow users to cash in funds
+
+* Send money through Padala Pay
+
+* Generate transaction receipts
+
+* Record and display account balances accurately
+
+
+
+System Requirements
+
+Before running the system, make sure the following are installed:
+
+* Python
+
+* Terminal or Command Prompt
+
+* Text Editor or IDE (VS Code, PyCharm, etc.)
+
+
+Files Included
+
+
+| File Name      | Description         |
+
+| -------------- | ------------------- |
+
+| `Banking_Card.py`   | Main Python Program |
+
+| `README.md`    | Project Description |
+
+| `UserGuide.md` | User Manual         |
+
+
+
+How to Run the System
+
+
+Step 1: Open Terminal or Command Prompt
+
+Navigate to the folder containing the Python file.
+
+Example:
+
+
+```bash
+
+cd Desktop/Banking Card
+
+```
+
+
+Step 2: Run the Program
+
+Type the following command:
+
+
+```bash
+
+py Banking_Card.py
+
+```
+
+
+or
+
+
+```bash
+
+python Banking_Card.py
+
+```
+
+
+Using the System
+
+Step 1: Login Authentication
+
+
+The system asks for:
+
+* Username
+
+* Password
+
+
+Allowed Users:
+
+* Raven
+
+* Carla
+
+* Jaren
+
+* Romero
+
+* Chin
+
+
+Example:
+
+```text
+
+Enter username: Romero
+
+Enter password: 12345
+
+```
+
+If the username and password are correct:
+
+
+```text
+
+Access granted! Welcome to the domain.
+
+```
+
+
+The system only allows 3 login attempts.
+
